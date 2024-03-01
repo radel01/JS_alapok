a9e1de1e6291aeb45cb4b319a1980f494ec472b8
Roszkopf Adel <133266580+radel01@users.noreply.github.com>
2024-03-01 11:42:34 +0100

    ora vege

--- script.js
+++ script.js
@@ -2,14 +2,57 @@ window.addEventListener("load", function () {
     elemekElerese1();
     elemekElerese2();
     elemekElerese3();
+    elemekElerese4();
+    elemekFormazasa1();
+    esemenyKezeles1();
+    elemekElerese7();
+    elemekElerese8();
+    elemekElerese9();
 });
 
 function elemekElerese1() {
-    /**Ide jön az első feladat */
+    const ELEM=document.querySelectorAll("section h2")[0]
+    console.log(ELEM.innerHTML)
 }
 function elemekElerese2() {
-    /**Ide jön az 2.  feladat */
+    const ELEM=document.querySelectorAll("#ide")[0]
+    ELEM.innerHTML="<p>Jó reggelt!</p>"
 }
 function elemekElerese3() {
-    /**Ide jön az 3. feladat */
+    const ELEM=document.querySelectorAll(".ide")[0]
+    ELEM.innerHTML="<p>Jó reggelt!</p>"
+}
+
+function elemekElerese4(){
+    const ELEM=document.getElementsByClassName("lista")
+    let txt=`<ul>`
+    for (let index = 0; index < 5; index++) {
+        let szam=Math.random()*20+10
+        txt+=`<li>${Math.floor(szam)}</li>` 
+    }txt+=`</ul>`
+    
+    ELEM[0].innerHTML=txt
+
+}
+
+function elemekFormazasa1(){
+    const ELEM=document.getElementsByClassName("lista")
+    ELEM[0].classList.add("formazott")
+}
+
+function esemenyKezeles1(){
+    const ELEM=document.getElementsByClassName("lista")[0]
+    ELEM.addEventListener("esemeny", fvenynev).
+}
+
+function elemekElerese7(){
+    
+}
+
+function elemekElerese8(){
+    
+}
+
+function elemekElerese9(){
+    
 }
