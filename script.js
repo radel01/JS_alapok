89cb214f5610a0d488c324fad584be80e840a595
Roszkopf Adel <133266580+radel01@users.noreply.github.com>
2024-03-22 12:01:36 +0100

    óra vége

--- script.js
+++ script.js
@@ -11,42 +11,49 @@ window.addEventListener("load", function () {
 });
 
 function elemekElerese1() {
-    const ELEM=document.querySelectorAll("section h2")[0]
+    const ELEM=$(".section h2")
     console.log(ELEM.innerHTML)
 }
 function elemekElerese2() {
-    const ELEM=document.querySelectorAll("#ide")[0]
-    ELEM.innerHTML="<p>Jó reggelt!</p>"
+    const ELEM=$("#ide")
+    ELEM.html("<p>Jó reggelt!</p>")
 }
 function elemekElerese3() {
-    const ELEM=document.querySelectorAll(".ide")[0]
-    ELEM.innerHTML="<p>Jó reggelt!</p>"
+    const ELEM=$(".ide")
+    ELEM.html("<p>Jó reggelt!</p>")
 }
 
 function elemekElerese4(){
-    const ELEM=document.getElementsByClassName("lista")
+    const ELEM=$(".lista")
     let txt=`<ul>`
     for (let index = 0; index < 5; index++) {
         let szam=Math.random()*20+10
         txt+=`<li>${Math.floor(szam)}</li>` 
     }txt+=`</ul>`
     
-    ELEM[0].innerHTML=txt
+    ELEM.eq(0).html(txt)
 
 }
 
 function elemekFormazasa1(){
-    const ELEM=document.getElementsByClassName("lista")
-    ELEM[0].classList.add("formazott")
+    const ELEM=$(".lista")
+    ELEM.eq(0).addClass("formazott")
 }
 
 function esemenyKezeles1(){
-    const ELEM=document.getElementsByClassName("lista")[0]
-    ELEM.addEventListener("esemeny", fvenynev)
+    const KATTINTASUTAN=$(".kattintasutan")
+    const ELEM=$(".lista")
+    ELEM.on("click", function(){
+        KATTINTASUTAN.eq(0).html(ELEM.html())
+        ELEM.empty()
+    })
+    
 }
 
 function elemekElerese7(){
+    const ELEM=$(".feladat_1")
     
+
 }
 
 function elemekElerese8(){
